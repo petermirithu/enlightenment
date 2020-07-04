63abd3165a6dd77b80dbf8923c50e1fff684c67a
petermirithu <pyra_m.k@yahoo.com>
2020-07-04 16:50:26 +0300

    deploy

--- models/index.js
+++ models/index.js
@@ -8,44 +8,30 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-
 let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-if (!global.hasOwnProperty('db')) {      
-
-  if (process.env.DATABASE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     5000,
-      host:     "heroku host",
-      logging:  true 
-    })
-  } else {
-    // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize(config.database, config.username, config.password, config);    
-  }
-
-  fs
-    .readdirSync(__dirname)
-    .filter(file => {
-      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-      const model = sequelize['import'](path.join(__dirname, file));
-      db[model.name] = model;
-    });
-
-  Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
-    }
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-  db.sequelize = sequelize;
-  db.Sequelize = Sequelize;
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
-}
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
