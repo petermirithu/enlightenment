b5a4a1937c16c287891c38153590e8c5b0006631
petermirithu <pyra_m.k@yahoo.com>
2020-07-15 22:56:39 +0300

    create profile route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../models").users;
+const Profile = require("../models").profiles;
 
 /* GET home page. */
+function isAuthenticated(req, res, next) {
+  if (req.user){
+    return next();      
+  }
+  else{
+    req.session.redirectTo = req.originalUrl; 
+    req.flash('success','Please Sign In if you have an Account'),            
+    res.redirect('/users/signup');     
+  }
+}
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Home' });
 });
@@ -52,6 +65,17 @@ router.get('/live_classes_2', function(req, res) {
 
 router.get('/live_classes_3', function(req, res) {
   res.render('live_classes_3', { title: 'Live Classes University' });
+});
+
+// sign up
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Sign Up' });
+});
+
+
+router.get('/profile', async(req, res) => {
+  let profile= await Profile.findOne({where: {userId: req.user.id}})
+  res.render('profile', { title: 'Profile',profile:profile});
 });
 
 module.exports = router;
